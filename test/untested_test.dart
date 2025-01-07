03b8aba42e741509e09c732a44f8ff15372a1fa4
Craig Hathaway <github@saropa.com>
2025-01-07 13:12:25 -0500

    package bumps

--- test/untested_test.dart
+++ test/untested_test.dart
@@ -1,3 +1,10 @@
+import 'package:test/test.dart';
+
 void main() {
   // TODO:
+  group('Do Nothing test for uploading version 0.0.1', () {
+    test('true equals true', () {
+      expect(true, true);
+    });
+  });
 }
